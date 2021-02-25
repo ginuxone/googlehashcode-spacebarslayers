9847fa9fa74af4e97606e3d33294b7f48ef52cfe
Andrea <andrea.0382@gmail.com>
2021-02-25 20:22:34 +0100

    Intersezione costruttore

--- Models/Intersezione.cs
+++ Models/Intersezione.cs
@@ -1,13 +1,15 @@
 using System;
 using System.Collections.Generic;
 using GoogleHashCode;
+
 class Intersezione {
     String id;
-    List<Strada> strade;
+    List<Strada> l_strade;
     private static int temp = 0;
 
-    public Intersezione () {
+    public Intersezione (List <Strada> l) {
         id = crea_id ();
+        l_strade = l;
     }
 
     private String crea_id () {
