b93bc6bb994e12b592ad0a883445c5a733276d60
GinoMilla <ginomilla@outlook.com>
2021-02-25 19:43:06 +0100

    Init Commit

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace GoogleHashCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            bool firstLine=true;
+            int DurataSimulazione=0;
+            fillData(firstLine,DurataSimulazione);
+            runSimulation();
+        }
+        private static void fillData(bool firstLine,int DurataSimulazione){
+            foreach (String line in System.IO.File.ReadLines("./InputFile/a.txt"))
+            {
+
+                if(firstLine){
+                    Console.WriteLine("First Line modddafoka");
+                    firstLine=false;
+                    DurataSimulazione=int.Parse(line.Split(" ")[0]);
+                }
+            }
+        }
+        private static void runSimulation(){
+
+        }
+    }
+}
