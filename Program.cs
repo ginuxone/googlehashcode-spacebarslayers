b28669956365dfd7934da13df4f3e88f8de8dd16
GinoMilla <ginomilla@outlook.com>
2021-02-25 20:33:44 +0100

    Auto stash before merge of "main" and "origin/main"

--- Program.cs
+++ Program.cs
@@ -14,10 +14,12 @@ namespace GoogleHashCode
             int n_macchine=0;
             int n_streets=0;
             int score=0;
-            fillData(ref firstLine,ref DurataSimulazione,ref n_macchine,ref n_Intersezioni,ref n_streets,ref score);
+            List<Strada> strade=new List<Strada>();
+            List<Intersezione> intersezioni=new List<Intersezione>();
+            fillData(ref firstLine,ref DurataSimulazione,ref n_macchine,ref n_Intersezioni,ref n_streets,ref score,ref strade,ref intersezioni);
             runSimulation();
         }
-        private static void fillData(ref bool firstLine,ref int DurataSimulazione,ref int n_macchine,ref int n_Intersezioni,ref int n_streets,ref int score){
+        private static void fillData(ref bool firstLine,ref int DurataSimulazione,ref int n_macchine,ref int n_Intersezioni,ref int n_streets,ref int score,ref List<Strada> strade,ref List<Intersezione> intersezioni){
             foreach (String line in System.IO.File.ReadLines("./InputFile/a.txt"))
             {
                 if(firstLine){
@@ -29,7 +31,19 @@ namespace GoogleHashCode
                     n_macchine=int.Parse(line.Split(" ")[3]);
                     score=int.Parse(line.Split(" ")[4]);
                 }else{
-                    
+                    foreach (Intersezione intersezione in intersezioni)
+                    {
+                        if(intersezione.id==int.Parse(line.Split(" ")[0])){
+                            
+                        }
+                    }
+                    strade.Add(new Strada(
+                        inizio:int.Parse(line.Split(" ")[0]),
+                        fine:int.Parse(line.Split(" ")[1]),
+                        nome:int.Parse(line.Split(" ")[2]),
+                        TPercorrenza:int.Parse(line.Split(" ")[3]),
+
+                    ));
                 }
             }
         }
