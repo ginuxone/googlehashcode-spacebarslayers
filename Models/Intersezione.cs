0c6d14c3c660b7bd559df79bae066e02478d8baa
Andrea <andrea.0382@gmail.com>
2021-02-25 20:32:20 +0100

    fix Intersezione

--- Models/Intersezione.cs
+++ Models/Intersezione.cs
@@ -5,7 +5,6 @@ using GoogleHashCode;
 class Intersezione {
     String id;
     List<Strada> l_strade;
-    private static int temp = 0;
 
     public Intersezione (String id, List <Strada> l) {
         this.id = id;
