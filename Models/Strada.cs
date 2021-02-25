6a157bdd7700e454d93e3194f2cc8b72aca463a0
Alberto <a.arrigoni5@studenti.unibg.it>
2021-02-25 20:01:04 +0100

    Strada update

---
+++ Models/Strada.cs
@@ -0,0 +1,13 @@
+namespace GoogleHashCode
+{
+    class Strada
+    {
+        public String nome;
+        public Semaforo semaforo;
+        public int lunghezza;
+        public Intersezione inizio;
+        public Intersezione fine;
+
+        public Strada() { }
+    }
+}
