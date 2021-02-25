b41d061a4d7f8b3cd27508e58f080bd206e70916
Andrea <andrea.0382@gmail.com>
2021-02-25 21:01:08 +0100

    Macchina: PassaSemaforo e  procede

--- Models/Macchina.cs
+++ Models/Macchina.cs
@@ -18,4 +18,27 @@ class Macchina {
         targa++;
         return res;
     }
+
+    public void procede () {   // strutturare il codice in modo che non si possa eseguire "procedere" e "passaSemaforo" assieme
+        if (stimato== 0) 
+            Console.WriteLine ("La macchina è gia al semaforo")
+        
+        else {
+            --stimato
+        }
+    }
+
+    public void passaSemaforo () {
+        if (stimato!= 0)
+            Console.WriteLine ("Macchina non ancora arrivata al semaforo");
+        
+        else {
+            percorso.RemoveAt(0);
+            if (percorso.Count > 0) 
+                stimato = percorso.Items(0);
+            
+            else 
+                stimato =  -1; // percorso concluso
+        }
+    }
 }
