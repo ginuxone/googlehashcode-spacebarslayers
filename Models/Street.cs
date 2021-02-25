76bd65502607248891cd10f5fc0234d49f45979a
Alberto <a.arrigoni5@studenti.unibg.it>
2021-02-25 19:55:00 +0100

    Implemented street

---
+++ Models/Street.cs
@@ -0,0 +1,11 @@
+namespace GoogleHashCode{
+    class Strada{
+        public String nome;
+        public Semaforo semaforo;
+        public int lunghezza;
+        public Intersezione intersezione1;
+        public Intersezione intersezione2;
+
+        public Strada(){}
+    }
+}
